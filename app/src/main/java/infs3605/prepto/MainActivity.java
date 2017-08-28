68b559abe22d78e026f40252d9d2bcd10d207e24
Andrew <chenzinee@gmail.com>
2017-08-28 19:02:41 +1000

    Initial Blank Project

---
+++ app/src/main/java/infs3605/prepto/MainActivity.java
@@ -0,0 +1,13 @@
+package infs3605.prepto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
