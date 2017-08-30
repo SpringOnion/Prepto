cdc7965d7b4c368caca0a31f097d07e32e81a83d
Andrew Chen <chenzinee@live.com>
2017-08-30 20:07:43 +1000

    Login Page and Empty Student Dashboard

---
+++ app/src/main/java/infs3605/prepto/LoginPage.java
@@ -0,0 +1,27 @@
+package infs3605.prepto;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPage extends AppCompatActivity {
+
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this, StudentDashboard.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
