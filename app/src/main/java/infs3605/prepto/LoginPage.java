192cbd76f35e58fe3e29e2346d169f1a0a9399ee
daphnenguyen <daphnenguyenn@gmail.com>
2017-09-06 16:36:10 +1000

    Daph commit - 6/9/17

--- app/src/main/java/infs3605/prepto/LoginPage.java
+++ app/src/main/java/infs3605/prepto/LoginPage.java
@@ -13,7 +13,7 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Button button = (Button) findViewById(R.id.btn_login);
         button.setOnClickListener(new View.OnClickListener() {
