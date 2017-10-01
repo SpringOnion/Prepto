6f7d2b39d3e003bc411df5099b5ea07ccb0b205a
amarkashyap <amar.kashyap095@gmail.com>
2017-10-01 15:24:54 +1100

    teTdhi

--- app/src/main/java/infs3605/prepto/DatabaseHelper.java
+++ app/src/main/java/infs3605/prepto/DatabaseHelper.java
@@ -2,6 +2,7 @@ package infs3605.prepto;
 
 import android.content.Context;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.renderscript.Sampler;
@@ -20,7 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_USERNAME = "username";
     public static final String COLUMN_PASS = "pass";
     private SQLiteDatabase db;
-    private static final String TABLE_CREATE = "create table contacts (id integer primary key not null auto increment, " + "" +
+    private static final String TABLE_CREATE = "create table contacts (id integer primary key not null, " + "" +
             "name text not null, email text not null, username text not null, pass text not null)";
 
 
@@ -39,12 +40,42 @@ db.execSQL(TABLE_CREATE);
     {
         db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
+
+        String query = "select * from contacts";
+        Cursor cursor = db.rawQuery(query, null);
+        int count = cursor.getCount();
+
+        values.put(COLUMN_ID, count);
         values.put(COLUMN_NAME , c.getName() );
         values.put(COLUMN_EMAIL , c.getEmail());
         values.put(COLUMN_USERNAME , c.getUsername());
         values.put(COLUMN_PASS , c.getPass());
 
         db.insert(TABLE_NAME , null, values);
+        db.close();
+
+    }
+
+    public String searchPass(String username) {
+        db = this.getReadableDatabase();
+        String query = "select username, pass from " + TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        String a, b;
+        b = "not found";
+        if (cursor.moveToFirst()) {
+            do {
+                a = cursor.getString(0);
+
+
+                if (a.equals(username)) {
+                    b = cursor.getString(1);
+                    break;
+                }
+            }
+            while (cursor.moveToNext());
+        }
+        return b;
+
 
     }
 
--- app/src/main/java/infs3605/prepto/LoginPage.java
+++ app/src/main/java/infs3605/prepto/LoginPage.java
@@ -5,23 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginPage extends AppCompatActivity {
 
-    public Button button;
+    public Button v;
+    DatabaseHelper helper = new DatabaseHelper(this);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login);}
 
-        Button button = (Button) findViewById(R.id.btn_login);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginPage.this, StudentDashboard.class);
-                startActivity(intent);
+        public void onButtonClick(View v)
+    {
+        if(v.getId() == R.id.BLogin)
+        {
+            EditText a = (EditText)findViewById(R.id.TFusername);
+            String str = a.getText().toString();
+            EditText b = (EditText)findViewById(R.id.TFpassword);
+            String pass = b.getText().toString();
+
+            String password = helper.searchPass(str);
+            if(pass.equals(password))
+            {
+                Intent i = new Intent (LoginPage.this, StudentDashboard.class);
+                i.putExtra("Username", str);
+                startActivity(i);
             }
-        });
+            else {
+                Toast temp = Toast.makeText(LoginPage.this, "Username and password dont match!", Toast.LENGTH_SHORT);
+                temp.show();
+            }
+
+        }
     }
-}
+
+
+            }
